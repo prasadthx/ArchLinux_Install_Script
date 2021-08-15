f16d78d99e94d0378db061c05e70ef8e4c7bb10f
prasad <prasadzore129@yahoo.com>
2021-08-15 22:04:31 +0530

    Setting shell variables

--- declare.sh
+++ declare.sh
@@ -2,14 +2,30 @@
 
 
 echo '
--------------------------------------------------------------------------
-        ____                   _
-       /_/\_\     ___    ___  | |___
-      /_/  \_\   |  _|  / __| |  __ \
-     /_/====\_\  | |   | |__  | |  \ \
-    /_/      \_\ |_|    \___| | |  | |
+-------------------------------------------------------------------------------
+        ____                   _             _                         __    __
+       / /\ \     ___    ___  | |___        | |     _    ___    _   _  \ \  / /
+      / /__\ \   |  _|  / __| |  __ \       | |    (_)  / _ \  | | | |  \ \/ /
+     / ______ \  | |   | |__  | |  \ \      | |__  | | | | | | | |_| |  / /\ \
+    / /      \ \ |_|    \___| | |  | |      |____| |_| |_| |_|  \___/  /_/  \_\
 
      Arch Linux Install Setup and Config
--------------------------------------------------------------------------
+-------------------------------------------------------------------------------
 '
-echo "Welcome to Arch Linux"
+
+#Set Up System Name:
+read -p "Enter Host Name [Default => ArchLinux]: " HOSTNAME
+HOSTNAME=${HOSTNAME:-"ArchLinux"}
+
+#Set Up UserName:
+read -p "Enter User Name [Default => archuser]: " HOSTNAME
+HOSTNAME=${HOSTNAME:-"archuser"}
+
+#Set Up Password:-
+read -p "Enter Host Name [Default => Arch1234]: " HOSTNAME
+HOSTNAME=${HOSTNAME:-"Arch1234"}
+
+#Set Up Language:-
+read -p "Enter Host Name [Default => en_IN.UTF-8]: " HOSTNAME
+HOSTNAME=${HOSTNAME:-"en_IN.UTF-8"}
+
